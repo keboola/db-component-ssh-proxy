4f1a4fe0014b3187b756542bdbb3bc922a95ac81
Ondra Jodas <ondrej.jodas@keboola.com>
2019-12-16 12:33:37 +0100

    docker push travis_tag

--- docker-push.sh
+++ docker-push.sh
@@ -3,4 +3,5 @@ set -e
 
 docker-compose build app
 docker login -u DOCKER_USERNAME -p DOCKER_PASSWORD
+docker push keboola/db-component-ssh-proxy:$TRAVIS_TAG
 docker push keboola/db-component-ssh-proxy:latest
