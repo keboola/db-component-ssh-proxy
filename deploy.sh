9c604e422a1af62e71524db1b1365752e2a11f29
Ondra Jodas <ondrej.jodas@keboola.com>
2019-12-16 14:02:00 +0100

    creating tag on dockerhub

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ docker-compose build app
 
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
-docker tag $REPOSITORY:$TRAVIS_TAG $REPOSITORY:$TRAVIS_TAG
+docker tag $REPOSITORY:latest $REPOSITORY:$TRAVIS_TAG
 
 docker push $REPOSITORY:$TRAVIS_TAG
 docker push $REPOSITORY:latest
